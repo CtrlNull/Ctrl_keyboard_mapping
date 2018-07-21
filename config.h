73fadce8e6cf7d325a8e391c8f482052516dcd9c
raphael@fonseca.tech <raphael@fonseca.tech>
2018-07-20 22:24:37 -0400

    rework of keys

--- config.h
+++ config.h
@@ -20,27 +20,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "config_common.h"
 
-/* Use I2C or Serial, not both */
-
 #define USE_SERIAL
-// #define USE_I2C
-
-/* Select hand configuration */
 
 #define MASTER_LEFT
-// #define MASTER_RIGHT
-// #define EE_HANDS
-
-#undef RGBLED_NUM
-#define RGBLIGHT_ANIMATIONS
-#define RGBLED_NUM 12
-#define RGBLIGHT_HUE_STEP 8
-#define RGBLIGHT_SAT_STEP 8
-#define RGBLIGHT_VAL_STEP 8
-
-//
-#define BACKLIGHT_LEVELS 3 // values 1-15(max)
-#define BACKLIGHT_BREATHING
-// #define BREATHING_PERIOD 6 // length in seconds
 
 #endif
--- keymap.c
+++ keymap.c
@@ -1,19 +1,16 @@
-#include "iris.h"
-#include "action_layer.h"
-#include "eeconfig.h"
+
+#include QMK_KEYBOARD_H
 
 extern keymap_config_t keymap_config;
 
 #define _QWERTY 0
 #define _LOWER 1
 #define _RAISE 2
-#define _ADJUST 16
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
   LOWER,
   RAISE,
-  ADJUST,
 };
 
 #define KC_ KC_TRNS
@@ -22,75 +19,72 @@ enum custom_keycodes {
 #define KC_LOWR LOWER
 #define KC_RASE RAISE
 #define KC_RST RESET
-#define KC_BL_S BL_STEP
 #define KC_DBUG DEBUG
-#define KC_RTOG RGB_TOG
-#define KC_RMOD RGB_MOD
-#define KC_RHUI RGB_HUI
-#define KC_RHUD RGB_HUD
-#define KC_RSAI RGB_SAI
-#define KC_RSAD RGB_SAD
-#define KC_RVAI RGB_VAI
-#define KC_RVAD RGB_VAD
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QWERTY] = LAYOUT_kc(
-  //,----+----+----+----+----+----.              ,----+----+----+----+----+----.
-     ESC , 1  , 2  , 3  , 4  , 5  ,                6  , 7  , 8  , 9  , 0  ,BSPC,
-  //|----+----+----+----+----+----|              |----+----+----+----+----+----|
-     TAB , Q  , W  , E  , R  , T  ,                Y  , U  , I  , O  , P  ,DEL ,
-  //|----+----+----+----+----+----|              |----+----+----+----+----+----|
-     LSFT, A  , S  , D  , F  , G  ,                H  , J  , K  , L  ,SCLN,QUOT,
-  //|----+----+----+----+----+----+----.    ,----|----+----+----+----+----+----|
-     LCTL, Z  , X  , C  , V  , B  ,HOME,     END , N  , M  ,COMM,DOT ,SLSH,RSFT,
-  //`----+----+----+--+-+----+----+----/    \----+----+----+----+----+----+----'
-                       LGUI,LOWR,ENT ,         SPC ,RASE,LALT
-  //                  `----+----+----'        `----+----+----'
+    /*
+     * ,-----+-----+-----+-----+-----+-----+                ,-----+-----+-----+-----+-----+-----+
+     * | ESC |  1  |  2  |  3  |  4  |  5  |                |  6  |  7  |  8  |  9  |  0  |  `  |
+     * |-----+-----+-----+-----+-----+-----+                +-----+-----+-----+-----+-----+-----+
+     * | TAB |  Q  |  W  |  E  |  R  |  T  |                |  Y  |  U  |  I  |  O  |  P  | BSPC|
+     * +-----+-----+-----+-----+-----+-----+                +-----+-----+-----+-----+-----+-----+
+     * | LCTL|  A  |  S  |  D  |  F  |  G  |                |  H  |  J  |  K  |  L  |  ;  |  '  |
+     * +-----+-----+-----+-----+-----+-----+-----.    ,-----+-----+-----+-----+-----+-----+-----+
+     * | LSFT|  Z  |  X  |  C  |  V  |  B  |     /    \     |  N  |  M  |  ,  |  .  |  /  | RSFT|
+     * +-----+-----+-----+--+--+-----+-----+ SPC/      \ ENT+-----+-----+--+--+-----+-----+-----+
+     *                      \ LGUI| LOWR|      /        \      | RASE| LALT/
+     *                      `-----+-----+-----'          `-----+-----+----'
+     */
+     ESC , 1  , 2  , 3  , 4  , 5  ,                6  , 7  , 8  , 9  , 0  , GRV,
+     TAB , Q  , W  , E  , R  , T  ,                Y  , U  , I  , O  , P  ,BSPC,
+     LCTL, A  , S  , D  , F  , G  ,                H  , J  , K  , L  ,SCLN,QUOT,
+     LSFT, Z  , X  , C  , V  , B  ,    ,         , N  , M  ,COMM,DOT ,SLSH,RSFT,
+                       LGUI,LOWR, SPC ,        ENT ,RASE,LALT
   ),
 
   [_LOWER] = LAYOUT_kc(
-  //,----+----+----+----+----+----.              ,----+----+----+----+----+----.
+    /*
+     * ,-----+-----+-----+-----+-----+-----+                ,-----+-----+-----+-----+-----+-----+
+     * |  ~  |  !  |  @  |  #  |  $  |  %  |                |  ^  |  &  |  *  |  (  |  )  | BSPC|
+     * |-----+-----+-----+-----+-----+-----+                +-----+-----+-----+-----+-----+-----+
+     * | RST |  1  |  2  |  3  |  5  |  5  |                |  6  |  7  |  8  |  9  |  0  |     |
+     * +-----+-----+-----+-----+-----+-----+                +-----+-----+-----+-----+-----+-----+
+     * | DEL |     | LEFT| RGHT| UP  |  [  |                |  ]  |  4  |  5  |  6  |  +  |  |  |
+     * +-----+-----+-----+-----+-----+-----+-----.    ,-----+-----+-----+-----+-----+-----+-----+
+     * |     |     |     |     | DOWN|  {  |     /    \     |  }  |  1  |  2  |  3  |  -  |     |
+     * +-----+-----+-----+--+--+-----+-----+ DEL/      \ DEL+-----+-----+--+--+-----+-----+-----+
+     *                      \     |     |      /        \      |     | 0   /
+     *                      `-----+-----+-----'          `-----+-----+----'
+     */
      TILD,EXLM, AT ,HASH,DLR ,PERC,               CIRC,AMPR,ASTR,LPRN,RPRN,BSPC,
-  //|----+----+----+----+----+----|              |----+----+----+----+----+----|
      RST , 1  , 2  , 3  , 4  , 5  ,                6  , 7  , 8  , 9  , 0  ,    ,
-  //|----+----+----+----+----+----|              |----+----+----+----+----+----|
      DEL ,    ,LEFT,RGHT, UP ,LBRC,               RBRC, P4 , P5 , P6 ,PLUS,PIPE,
-  //|----+----+----+----+----+----+----.    ,----|----+----+----+----+----+----|
-     BL_S,    ,    ,    ,DOWN,LCBR,LPRN,     RPRN,RCBR, P1 , P2 , P3 ,MINS,    ,
-  //`----+----+----+--+-+----+----+----/    \----+----+----+----+----+----+----'
+         ,    ,    ,    ,DOWN,LCBR,    ,         ,RCBR, P1 , P2 , P3 ,MINS,    ,
                            ,    ,DEL ,         DEL ,    , P0
-  //                  `----+----+----'        `----+----+----'
   ),
 
   [_RAISE] = LAYOUT_kc(
-  //,----+----+----+----+----+----.              ,----+----+----+----+----+----.
+    /*
+     * ,-----+-----+-----+-----+-----+-----+                ,-----+-----+-----+-----+-----+-----+
+     * | F12 |  F1 |  F2 |  F3 |  F4 |  F5 |                |  F6 |  F7 |  F8 |  F9 | F10 | F11 |
+     * |-----+-----+-----+-----+-----+-----+                +-----+-----+-----+-----+-----+-----+
+     * |     |  !  |  @  |  #  |  $  |  %  |                |  ^  |  &  |  *  |  (  |  )  |     |
+     * +-----+-----+-----+-----+-----+-----+                +-----+-----+-----+-----+-----+-----+
+     * |     | Prev| Next| Vol+| PgUp|  _  |                |  =  | Home|     |     |  +  |  \  |
+     * +-----+-----+-----+-----+-----+-----+-----.    ,-----+-----+-----+-----+-----+-----+-----+
+     * | Mute| Stop| Play| Vol-| PgDn|  -  |     /    \     |  +  | End |     |     |     |Debug|
+     * +-----+-----+-----+--+--+-----+-----+    /      \    +-----+-----+--+--+-----+-----+-----+
+     *                      \     |     |      /        \      |     | 0   /
+     *                      `-----+-----+-----'          `-----+-----+----'
+     */
      F12 , F1 , F2 , F3 , F4 , F5 ,                F6 , F7 , F8 , F9 ,F10 ,F11 ,
-  //|----+----+----+----+----+----|              |----+----+----+----+----+----|
          ,EXLM, AT ,HASH,DLR ,PERC,               CIRC,AMPR,ASTR,LPRN,RPRN,    ,
-  //|----+----+----+----+----+----|              |----+----+----+----+----+----|
          ,MPRV,MNXT,VOLU,PGUP,UNDS,               EQL ,HOME,    ,    ,    ,BSLS,
-  //|----+----+----+----+----+----+----.    ,----|----+----+----+----+----+----|
-     MUTE,MSTP,MPLY,VOLD,PGDN,MINS,    ,         ,PLUS,END ,    ,    ,    ,    ,
-  //`----+----+----+--+-+----+----+----/    \----+----+----+----+----+----+----'
+     MUTE,MSTP,MPLY,VOLD,PGDN,MINS,    ,         ,PLUS,END ,    ,    ,    ,DBUG,
                            ,    ,    ,             ,    ,
-  //                  `----+----+----'        `----+----+----'
-  ),
-
-  [_ADJUST] = LAYOUT_kc(
-  //,----+----+----+----+----+----.              ,----+----+----+----+----+----.
-         ,    ,    ,    ,    ,    ,                   ,    ,    ,    ,    ,    ,
-  //|----+----+----+----+----+----|              |----+----+----+----+----+----|
-     RTOG,RMOD,RHUI,RSAI,RVAI,    ,                   ,    ,    ,    ,    ,    ,
-  //|----+----+----+----+----+----|              |----+----+----+----+----+----|
-         ,DBUG,RHUD,RSAD,RVAD,    ,                   ,    ,    ,    ,    ,    ,
-  //|----+----+----+----+----+----+----.    ,----|----+----+----+----+----+----|
-     BL_S,RST ,    ,    ,    ,    ,    ,         ,    ,    ,    ,    ,    ,    ,
-  //`----+----+----+--+-+----+----+----/    \----+----+----+----+----+----+----'
-                           ,    ,    ,             ,    ,
-  //                  `----+----+----'        `----+----+----'
   )
-
 };
 
 void persistent_default_layer_set(uint16_t default_layer) {
@@ -109,28 +103,20 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case LOWER:
       if (record->event.pressed) {
         layer_on(_LOWER);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+        update_tri_layer(_LOWER, _RAISE, _QWERTY);
       } else {
         layer_off(_LOWER);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+        update_tri_layer(_LOWER, _RAISE, _QWERTY);
       }
       return false;
       break;
     case RAISE:
       if (record->event.pressed) {
         layer_on(_RAISE);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+        update_tri_layer(_LOWER, _RAISE, _QWERTY);
       } else {
         layer_off(_RAISE);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
-      }
-      return false;
-      break;
-    case ADJUST:
-      if (record->event.pressed) {
-        layer_on(_ADJUST);
-      } else {
-        layer_off(_ADJUST);
+        update_tri_layer(_LOWER, _RAISE, _QWERTY);
       }
       return false;
       break;
